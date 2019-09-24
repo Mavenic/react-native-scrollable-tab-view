8c501c0a35f44d751da31da9bbf53d4d056b0624
Saket Kumar <Mavenic@users.noreply.github.com>
2019-09-24 23:03:00 +0530

    Update DefaultTabBar.js

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -70,7 +70,7 @@ const DefaultTabBar = createReactClass({
       outputRange: [0,  containerWidth / numberOfTabs],
     });
     return (
-      <View style={[styles.tabs, {backgroundColor: this.props.backgroundColor, }, this.props.style, ]}>
+      <View style={[styles.tabs, this.props.style]}>
         {this.props.tabs.map((name, page) => {
           const isTabActive = this.props.activeTab === page;
           const renderTab = this.props.renderTab || this.renderTab;
