ee93e7190be5dad3302c19b583c9124c30ebb9be
Saket Kumar <Mavenic@users.noreply.github.com>
2019-10-11 20:56:05 +0530

    Update Button.android.js

--- Button.android.js
+++ Button.android.js
@@ -2,17 +2,16 @@ const React = require('react');
 const ReactNative = require('react-native');
 const {
   TouchableNativeFeedback,
+  TouchableWithoutFeedback
   View,
 } = ReactNative;
 
 const Button = (props) => {
-  return <TouchableNativeFeedback
-    delayPressIn={0}
-    background={TouchableNativeFeedback.SelectableBackground()} // eslint-disable-line new-cap
-    {...props}
-  >
+  return (
+  <TouchableWithoutFeedback {...props}>
     {props.children}
-  </TouchableNativeFeedback>;
+  </TouchableWithoutFeedback> 
+  );
 };
 
 module.exports = Button;
